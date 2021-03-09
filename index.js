05466891c72791a66e089417ecd72bb0c039511a
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-08 19:06:20 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(`${name}`);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(`${name}`);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, `${name}`];
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats = [`${name}`, ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(-0, 2);
+    console.log(copyOfCats);
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1);
+    console.log(copyOfCats);
+    return copyOfCats;
+}
